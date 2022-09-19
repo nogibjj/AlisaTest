f311974972a85c3170121792f2a08ef355835c0a
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-09-19 02:46:00 +0000

    change na2

--- dblib/querydb.py
+++ dblib/querydb.py
@@ -10,7 +10,7 @@ def querydb(query="American"):
     ) as connection:
 
         with connection.cursor() as cursor:
-            cursor.execute("SELECT Name FROM default.artists_1_csv WHERE spark_catalog.default.artists_1_csv.Nationality="+query)
+            cursor.execute("SELECT Name FROM default.artists_1_csv WHERE Nationality="+query)
             result = cursor.fetchall()
 
         for row in result:
